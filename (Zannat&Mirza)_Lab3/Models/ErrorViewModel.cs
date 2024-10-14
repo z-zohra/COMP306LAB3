10e79c11125e0f85b1e52d9b1b89a563a4a5dfc8
zzannat <zzannat@my.centennialcollege.ca>
2024-10-14 17:15:53 -0400

    initial commit

---
+++ (Zannat&Mirza)_Lab3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _Zannat_Mirza__Lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
